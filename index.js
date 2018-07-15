3d874615d5edd9670834ebe7340fa5fa7c2d5569
Alexandre Trovato <alexandre.trovato@gmail.com>
2018-07-15 14:45:12 +0200

    Try command with sentence

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 
 module.exports = function(sails) {
+    var changeChannel = require('./lib/change_channel.js');
     var exec = require('./lib/exec.js');
     var setup = require('./lib/setup.js');
     var install = require('./lib/install.js');
-    var command = require('./lib/command.js');
+    var command = require('./lib/lirc.command.js');
 
     return {
-        exec: exec,
-        setup: setup,
-        install: install,
-        command: command
-    }
-};
+        changeChannel: changeChannel,
+    
