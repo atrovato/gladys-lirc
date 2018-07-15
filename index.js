546375fb59bd416a134db181c021fac3155aafc1
Alexandre Trovato <alexandre.trovato@gmail.com>
2018-07-15 13:19:39 +0200

    Change channel
    Allow to change TV channel using LIRC.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+
 module.exports = function(sails) {
     var exec = require('./lib/exec.js');
+    var setup = require('./lib/setup.js');
+    var install = require('./lib/install.js');
+    var command = require('./lib/command.js');
 
     return {
-        exec: exec
+        exec: exec,
+        setup: setup,
+        install: install,
+        command: command
     }
 };
