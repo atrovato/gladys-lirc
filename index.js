c202a93709aaf1f8ae94f5a5e72dd92e05a04138
Alexandre Trovato <alexandre.trovato@gmail.com>
2018-07-15 14:49:57 +0200

    Fix index

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 
 module.exports = function(sails) {
-    var changeChannel = require('./lib/change_channel.js');
     var exec = require('./lib/exec.js');
     var setup = require('./lib/setup.js');
     var install = require('./lib/install.js');
     var command = require('./lib/lirc.command.js');
 
     return {
-        changeChannel: changeChannel,
-    
+        exec: exec,
+        setup: setup,
+        install: install,
+        command: command
+    }
+};
