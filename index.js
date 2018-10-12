24f7607208741f06f620e6a1981c850c2eed01a8
Alexandre Trovato <alexandre.trovato@gmail.com>
2018-10-12 21:39:29 +0200

    Apply eslint

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 module.exports = function(sails) {
-    var exec = require('./lib/exec.js');
-    var setup = require('./lib/setup.js');
-    var install = require('./lib/install.js');
-    var command = require('./lib/lirc.command.js');
+  var exec = require('./lib/exec.js');
+  var setup = require('./lib/setup.js');
+  var install = require('./lib/install.js');
+  var command = require('./lib/lirc.command.js');
 
-    return {
-        exec: exec,
-        setup: setup,
-        install: install,
-        command: command
-    }
+  return {
+    exec: exec,
+    setup: setup,
+    install: install,
+    command: command
+  };
 };
