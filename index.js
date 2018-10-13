552f174a3e1494b414d31009a3e53b2d80956b75
Alexandre Trovato <alexandre.trovato@gmail.com>
2018-10-13 11:39:30 +0200

    Complete Gladys TV API

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const television = {
   getState : require('./lib/lirc.getState.js'),
   switchState : require('./lib/lirc.switchState.js'),
   getCurrentChannel : require('./lib/lirc.getCurrentChannel.js'),
-  setChannel : require('./lib/lirc.setChannel.js')
+  setChannel : require('./lib/lirc.setChannel.js'),
+  fastForward : require('./lib/lirc.fastForward.js'),
+  rewind : require('./lib/lirc.rewind.js'),
+  mute : require('./lib/lirc.mute.js'),
+  volumeUp : require('./lib/lirc.volumeUp.js'),
+  volumeDown : require('./lib/lirc.volumeDown.js'),
+  play : require('./lib/lirc.play.js'),
+  pause : require('./lib/lirc.pause.js'),
+  stop : require('./lib/lirc.stop.js')
 };
 
 /**
